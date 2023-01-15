c982caf176d3986752c29a18bf5332990677b8bc
Diego Hernandez Forero <decreativosweb@gmail.com>
2023-01-14 21:37:19 -0500

    Desarrollo al reto del video https://www.youtube.com/watch?v=P03OPwNmDKs~

--- main.js
+++ main.js
@@ -1,7 +1,17 @@
-const input = document.querySelector("input");
-const addBtn = document.querySelector(".btn-add");
-const ul = document.querySelector("ul");
-const empty = document.querySelector(".empty");
+//Capturo los elementos con los que voy a interactuar
+const input = document.querySelector("input");       //Capturo el input y lo almaceno en una constante
+const addBtn = document.querySelector(".btn-add");   //Capturo el btn-add y lo almaceno en una constante
+const ul = document.querySelector("ul");             //Capturo el ul y lo almaceno en una constante 
+const empty = document.querySelector(".empty");      //Capturo el .empty y lo almaceno en una constante
+const eliminarAll = document.querySelector(".btn-eliminar-all"); // <--- codigo adicional para el reto
+
+eliminarAll.style.display = "none";  // <--- codigo adicional para el reto
+
+eliminarAll.addEventListener("click", () => { // <--- codigo adicional para el reto
+  ul.innerHTML = "";                          // <--- codigo adicional para el reto
+  eliminarAll.style.display = "none";         // <--- codigo adicional para el reto
+});
+
 
 addBtn.addEventListener("click", (e) => {
   e.preventDefault();
@@ -19,14 +29,17 @@ addBtn.addEventListener("click", (e) => {
 
     input.value = "";
     empty.style.display = "none";
+    eliminarAll.style.display = "block";  // <--- codigo adicional para el reto
   }
 });
+
 
 function addDeleteBtn() {
   const deleteBtn = document.createElement("button");
 
   deleteBtn.textContent = "X";
   deleteBtn.className = "btn-delete";
+
 
   deleteBtn.addEventListener("click", (e) => {
     const item = e.target.parentElement;
@@ -36,6 +49,7 @@ function addDeleteBtn() {
 
     if (items.length === 0) {
       empty.style.display = "block";
+      eliminarAll.style.display = "none";  // <--- codigo adicional para el reto
     }
   });
 
